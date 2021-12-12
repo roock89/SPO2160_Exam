9d2116d7120877e951c9c29886c3c6f19c8ecb26
roock89 <77836346+roock89@users.noreply.github.com>
2021-12-12 19:10:20 +0100

    leaderboard update

--- SPO2160_Exam/Assets/Scripts/UI/LeaderboardController.cs
+++ SPO2160_Exam/Assets/Scripts/UI/LeaderboardController.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+
 
 public class LeaderboardController : MonoBehaviour
 {
     public GameObject _highScorePanel;
     public Transform ScrollContent;
     private RectTransform ContentSize;
+    public string HighscoreURL;
+    public string[] scores;
     
-    private readonly Vector3 StartPos = new Vector3(0, 897.4f, 0); // highscore starting pos
+    private readonly Vector3 StartPos = new Vector3(0,660,0); // highscore starting pos
     // Start is called before the first frame update
     
     void OnEnable()
@@ -23,20 +27,70 @@ public class LeaderboardController : MonoBehaviour
 
     private void loadHighScores()
     {
+        StartCoroutine(loadHighScores(HighscoreURL));
+    }
+
+    public IEnumerator loadHighScores(string uri)
+    {
+        Debug.Log("Test score script: " + uri);
+
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+
+        UnityWebRequest www = UnityWebRequest.Post(uri, formData);
+        yield return www.SendWebRequest();
+        // Debug.Log(www.downloadHandler.text);
+
+        // Split string using space, semi colon, comma, dot
+        if (www.downloadHandler.text.Length > 1)
+        {
+            Debug.Log("More than one character received");
+            //Split string into two parts using string.split
+            scores = www.downloadHandler.text.Split(';');
+        }
+        else
+        {
+            scores = new string[1];
+            scores[0] = www.downloadHandler.text;
+        }
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+
         ContentSize = ScrollContent.GetComponent<RectTransform>();
         for(int i = 0; i < ScrollContent.childCount; i++)
         {
             Destroy(ScrollContent.GetChild(i).gameObject);
         }
-        ContentSize.sizeDelta = new Vector2(3237, 0); // resetting scroll content size
-        int ChallengerNumb = 10; // number of scores loading in
+        ContentSize.sizeDelta = new Vector2(3237, 1520.5f); // resetting scroll content size
+        int ChallengerNumb = scores.Length; // number of scores loading in
         ContentSize.sizeDelta = new Vector2(3237, (ChallengerNumb-1) * 230f); // ScrollContent total size
-        for(int i = 0; i < ChallengerNumb; i++)
+        for(int i = 0; i < ChallengerNumb -1 && ChallengerNumb > 0; i++)
         {
             GameObject spawn = Instantiate(_highScorePanel);
             spawn.transform.SetParent(ScrollContent, false);
             spawn.transform.localPosition = StartPos + new Vector3(0, i, 0) * -200; // position delta
+
+            HighScorePanelController controller = spawn.GetComponent<HighScorePanelController>();
+            string[] _split = scores[i].Split(':');
+            if(_split.Length > 1)
+            {
+                controller.PlayerText.text = _split[0];
+                controller.ScoreText.text = _split[1]; 
+            }
+            else
+                controller.PlayerText.text = _split[0];
         }
         ScrollContent.transform.position -= new Vector3(0, 100 ,0); // correct starting position. might need a bigger number if loading more than 10 highscores
+        
+        // if(scores != null)
+        // {
+        //     foreach(string score in scores)
+        //     {
+        //         Debug.Log(score);
+        //     }
+        // }
     }
+
 }
