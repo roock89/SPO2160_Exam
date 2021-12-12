b7681f4bcd8330e790d9b5a85ef912c063d84017
Shyronzo <james-sst@hotmail.com>
2021-12-12 11:50:13 +0100

    Inputsytem pushed in, to maybe let us make replays
    also enabled both input systems

---
+++ SPO2160_Exam/Assets/Scripts/InputRecorder.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputRecorder : MonoBehaviour
+{
+    public bool simulateOriginalTimingOnReplay = true;
+    public bool startRecordingWhenEnabled = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartReplay()
+    {
+
+    }
+    public void StartCapture()
+    {
+
+    }
+}
