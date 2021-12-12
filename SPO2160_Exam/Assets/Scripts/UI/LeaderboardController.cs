08b95b1fb5034a5cdcf852d751876b7e536b7626
roock89 <77836346+roock89@users.noreply.github.com>
2021-12-12 14:43:11 +0100

    leaderboard UI ready for implementation

--- SPO2160_Exam/Assets/Scripts/UI/LeaderboardController.cs
+++ SPO2160_Exam/Assets/Scripts/UI/LeaderboardController.cs
@@ -6,15 +6,21 @@ public class LeaderboardController : MonoBehaviour
 {
     public GameObject _highScorePanel;
     public Transform ScrollContent;
-    private readonly Vector3 StartPos = new Vector3(0, 641, 0); 
+    private RectTransform ContentSize;
+    
+    private readonly Vector3 StartPos = new Vector3(0, 897.4f, 0); // highscore starting pos
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < 10; i++)
+        ContentSize = ScrollContent.GetComponent<RectTransform>();
+        int ChallengerNumb = 10; // number of scores loading in
+        ContentSize.sizeDelta = new Vector2(3237, (ChallengerNumb-1) * 230f); // ScrollContent total size
+        for(int i = 0; i < ChallengerNumb; i++)
         {
             GameObject spawn = Instantiate(_highScorePanel);
             spawn.transform.SetParent(ScrollContent, false);
-            spawn.transform.localPosition = StartPos + new Vector3(0, i, 0) * -200;
+            spawn.transform.localPosition = StartPos + new Vector3(0, i, 0) * -200; // position delta
         }
+        ScrollContent.transform.position -= new Vector3(0, 100 ,0); // correct starting position. might need a bigger number if loading more than 10 highscores
     }
 }
