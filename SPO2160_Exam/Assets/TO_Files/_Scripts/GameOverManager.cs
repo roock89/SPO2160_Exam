9407bfa539d667106956daf81b5125048c4fccc7
Quick-Bolt <nuno.gnu1@gmail.com>
2021-12-12 18:14:39 +0100

    *EPIC* changes, everything works baby :baby:

--- SPO2160_Exam/Assets/TO_Files/_Scripts/GameOverManager.cs
+++ SPO2160_Exam/Assets/TO_Files/_Scripts/GameOverManager.cs
@@ -3,22 +3,55 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.Networking;
-
+using System.Collections.Generic;
 
 public class GameOverManager : MonoBehaviour
 {
-    public string logoutUrl;
+    public string logoutUrl, savingScoreURL;
     public int attemptsUsed;
-
-    public Text feedbackText;
+    public Text feedbackText, scoreText;
 
     // Start is called before the first frame update
     void Start()
     {
         attemptsUsed = PlayerPrefs.GetInt("AttemptsUsed");
         feedbackText.text = "You guessed " + attemptsUsed.ToString() + " times";
+        StartCoroutine(SavingScore(savingScoreURL, attemptsUsed.ToString()));
     }
 
+    public IEnumerator SavingScore(string uri, string score)
+    {
+        Debug.Log("Test score script: " + uri);
+
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+
+        // Add username and password as separate form data sections!
+        formData.Add(new MultipartFormDataSection("score", score));
+
+        UnityWebRequest www = UnityWebRequest.Post(uri, formData);
+        yield return www.SendWebRequest();
+        Debug.Log(www.downloadHandler.text);
+
+        // Split string using space, semi colon, comma, dot
+        if (www.downloadHandler.text.Length > 1)
+        {
+            Debug.Log("More than one character received");
+            //Split string into two parts using string.split
+            string[] feedback = www.downloadHandler.text.Split(';');
+            score = feedback[0].ToString();
+        }
+        else
+        {
+            score = www.downloadHandler.text;
+        }
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+
+        scoreText.text = score;
+    }
 
     public void RestartGame()
     {
