7754fd25357adfde4f3a6531fe310d4c14ae0fa4
Quick-Bolt <nuno.gnu1@gmail.com>
2021-12-13 19:33:20 +0100

    canvas now a prefab :car:

--- SPO2160_Exam/Assets/Scripts/GameManager.cs
+++ SPO2160_Exam/Assets/Scripts/GameManager.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     private static GameManager iAmTheManager;
     public GhostHolder ghostInputData;
     public string GhostDataURL;
+
     // Start is called before the first frame update
     void Start()
     {
