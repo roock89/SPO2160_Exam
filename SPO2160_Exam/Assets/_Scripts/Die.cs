fc5c7d5c625bfd0f02d445b45a2db774353f88d9
StormTruck <77848378+StormTruck@users.noreply.github.com>
2021-12-15 19:15:09 +0100

    Applied PlayerCar prefab changes + added condition for falling off 3 times

--- SPO2160_Exam/Assets/_Scripts/Die.cs
+++ SPO2160_Exam/Assets/_Scripts/Die.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class Die : MonoBehaviour
 {
+    public int timesFallen = 0;
+
     //OnTrigger collects the RigidBody and DieAndRespawn script from the player
     void OnTriggerEnter(Collider other)
     {
-        Rigidbody rb = other.GetComponent<Rigidbody>();
-        DieAndRespawn respawn = other.GetComponent<DieAndRespawn>();
-
-        //If the DieAndRespawn is found, run the HitAndRespawn method of that script and set the players velocity to 0
-        if (respawn != null) 
+        if (other.tag == "Player")
         {
-            respawn.HitAndRespawn();
-            rb.velocity = Vector3.zero;
-        }    
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            DieAndRespawn respawn = other.GetComponent<DieAndRespawn>();
+            timesFallen++;
+
+            //If the DieAndRespawn is found, run the HitAndRespawn method of that script and set the players velocity to 0
+            if (respawn != null)
+            {
+                respawn.HitAndRespawn();
+                rb.velocity = Vector3.zero;
+            }
+            if (timesFallen >= 3)
+            {
+                // Activate Game Over
+                // Freeze game??
+            }
+        }
     }
 }
